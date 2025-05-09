ceeea4974ae66549d0b8adce4679034ac18404ea
James Anderson <james.r.anderson@utah.edu>
2025-05-09 16:56:44 -0700

    Fixing docker and deployment issues

---
+++ libraries/segmentation_grpc/segmentation_grpc/__init__.py
@@ -0,0 +1,7 @@
+from segmentation_pb2 import (SegmentationRequest,
+                              SegmentationResponse,
+                              Point, Polygon,
+                              SegmentationService)
+
+from segmentation_pb2_grpc import (SegmentationServiceServicer,
+                                   SegmentationService)
