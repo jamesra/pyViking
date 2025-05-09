6b9a3758e59d0fcb3007fa5600419a82600e6272
James Anderson <james.r.anderson@utah.edu>
2025-05-09 16:35:34 -0700

    Added a --force flag to require compilation

--- libraries/segmentation_grpc/segmentation_grpc/__main__.py
+++ libraries/segmentation_grpc/segmentation_grpc/__main__.py
@@ -6,12 +6,20 @@ python -m segmentation_grpc
 """
 
 import sys
+import argparse
 from segmentation_grpc.generate_grpc import generate_grpc_code
 
-def main():
+def main(*args):
+    """Main entry point for the segmentation service."""
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Compile gRPC code for the segmentation service.')
+    parser.add_argument('--force', action='store_true',
+                        help='Regenerate gRPC code even if it already exists')
+    args = parser.parse_args()
+
     """Generate the gRPC code when the package is run as a module."""
     print("Generating gRPC code...")
-    if generate_grpc_code():
+    if generate_grpc_code(args.force):
         print("gRPC code generation successful.")
         return 0
     else:
